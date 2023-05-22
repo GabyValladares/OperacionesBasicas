28b0d269dc942b747b7c63de7580d6eda2aa8cb1
GabyValladares <gvalladares@ist17dejulio.edu.ec>
2023-05-22 18:07:39 -0500

    Métodos sumar,regresar,ir

--- app/src/main/java/com/example/operacionesbasicas/MainActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.operacionesbasicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //MÉTODO PARA CAMBIAR DE ACTIVITY
+    public void irSuma(View view){
+        Intent intent=new Intent(this,SumaActivity.class);
+        view.getContext().startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
@@ -0,0 +1,30 @@
+package com.example.operacionesbasicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SumaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+    }
+
+    public void regresarMenu(View view){
+        this.finish();
+    }
+    public void Sumar(View view){
+        EditText numero1= (EditText) findViewById(R.id.txtNumero1);
+        EditText numero2= (EditText) findViewById(R.id.txtNumero2);
+        TextView total=findViewById(R.id.txtEResultado);
+        double resultado=
+                Double.parseDouble(numero1.getText().toString())+Double.parseDouble(numero2.getText().toString());
+
+        total.setText(resultado+"");
+    }
+}
